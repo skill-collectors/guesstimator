74c1529065271d76c85afbc70b9f7e54a9e2f147
Dave Burke <dburke84@gmail.com>
2022-05-26 05:10:45 -0500

    Use tabs consistently in frontend project
    This is consistent with Svelte idioms: https://github.com/sveltejs/svelte/blob/master/.editorconfig
    
    We can always change it later.

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,9 +1,9 @@
 <script>
-  import "virtual:windi.css"
+	import "virtual:windi.css"
 
-  // if you want to enable windi devtools
-  import { browser } from "$app/env";
-  if (browser) import("virtual:windi-devtools")
+	// if you want to enable windi devtools
+	import { browser } from "$app/env";
+	if (browser) import("virtual:windi-devtools")
 </script>
 <div class="h-full flex flex-col">
 	<header>Header</header>
