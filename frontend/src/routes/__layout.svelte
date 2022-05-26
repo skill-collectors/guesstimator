05a8e07e7b877cfccfb7a35db464616f9a3746ee
Dave Burke <dburke84@gmail.com>
2022-05-26 05:25:11 -0500

    Ignore windi devtools missing types

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -3,6 +3,7 @@
 
 	// if you want to enable windi devtools
 	import { browser } from "$app/env";
+	// @ts-ignore See https://github.com/windicss/vite-plugin-windicss/issues/294
 	if (browser) import("virtual:windi-devtools")
 </script>
 <div class="h-full flex flex-col">
