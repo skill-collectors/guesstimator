b88554609e7833416104564669833e6032b22488
Dave Burke <dburke84@gmail.com>
2022-05-26 22:21:21 -0500

    Add sample component test

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -4,9 +4,11 @@
   // if you want to enable windi devtools
   import { browser } from "$app/env";
   if (browser) import("virtual:windi-devtools")
+
+	import Header from '$lib/Header.svelte'
 </script>
 <div class="h-full flex flex-col">
-	<header>Header</header>
+	<Header></Header>
 	<main class="flex-grow">
 		<slot></slot>
 	</main>
---
+++ frontend/tests/unit/lib/Header.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest';
+import { render } from '@testing-library/svelte';
+import Header from '$lib/Header.svelte';
+
+describe("Header", () => {
+
+    it('Should say "Welcome to SvelteKit"', () => {
+        const {getByText} = render(Header);
+
+        expect(() => getByText(/welcome to sveltekit/i)).not.toThrow();
+    })
+})
+
