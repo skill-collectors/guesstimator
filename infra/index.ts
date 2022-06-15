3e42d4e82c5ed41697f9894bb8f5fb2a7d0436d2
Dave Burke <dburke84@gmail.com>
2022-06-14 19:56:11 -0500

    Copy infra code from rpn-calc

--- infra/index.ts
+++ infra/index.ts
@@ -1,7 +1,130 @@
+import * as pulumi from '@pulumi/pulumi'
 import * as aws from '@pulumi/aws'
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('my-bucket')
+const stack = pulumi.getStack()
+const subDomain = stack === 'prod' ? 'agile-poker' : `agile-poker-${stack}`
+const apexDomain = 'superfun.link'
+const domain = `${subDomain}.${apexDomain}`
 
-// Export the name of the bucket
-export const bucketName = bucket.id
+const tags = { iac: 'pulumi', project: 'agile-poker', stack }
+
+const siteBucket = new aws.s3.Bucket('siteBucket', {
+  bucket: domain,
+  website: {
+    indexDocument: 'index.html'
+  },
+  tags
+})
+
+const hostedZone = aws.route53.getZone({
+  name: apexDomain
+})
+
+const certificate = new aws.acm.Certificate('certificate', {
+  domainName: domain,
+  validationMethod: 'DNS',
+  tags
+})
+
+const certificateValidationDomain = new aws.route53.Record('dnsRecordValidation', {
+  name: certificate.domainValidationOptions[0].resourceRecordName,
+  zoneId: hostedZone.then(hostedZone => hostedZone.zoneId),
+  type: certificate.domainValidationOptions[0].resourceRecordType,
+  records: [certificate.domainValidationOptions[0].resourceRecordValue],
+  ttl: 300
+})
+
+const certificateValidation = new aws.acm.CertificateValidation('certificateValidation', {
+  certificateArn: certificate.arn,
+  validationRecordFqdns: [certificateValidationDomain.fqdn]
+})
+
+const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity('originAccessIdentity', {
+  comment: 'this is needed to setup s3 polices and make s3 not public.'
+})
+
+const cdn = new aws.cloudfront.Distribution('cdn', {
+  enabled: true,
+  aliases: [domain],
+
+  origins: [
+    {
+      originId: siteBucket.arn,
+      domainName: siteBucket.bucketDomainName,
+      s3OriginConfig: {
+        originAccessIdentity: originAccessIdentity.cloudfrontAccessIdentityPath
+      }
+    }
+  ],
+
+  defaultRootObject: 'index.html',
+
+  defaultCacheBehavior: {
+    targetOriginId: siteBucket.arn,
+
+    viewerProtocolPolicy: 'redirect-to-https',
+    allowedMethods: ['GET', 'HEAD', 'OPTIONS'],
+    cachedMethods: ['GET', 'HEAD', 'OPTIONS'],
+
+    forwardedValues: {
+      cookies: { forward: 'none' },
+      queryString: false
+    },
+
+    minTtl: 0,
+    defaultTtl: 300,
+    maxTtl: 300
+  },
+
+  priceClass: 'PriceClass_100',
+
+  restrictions: {
+    geoRestriction: {
+      restrictionType: 'none'
+    }
+  },
+
+  viewerCertificate: {
+    acmCertificateArn: certificateValidation.certificateArn,
+    sslSupportMethod: 'sni-only'
+  },
+
+  tags
+})
+
+const record = new aws.route53.Record('dnsRecord', {
+  name: domain,
+  zoneId: hostedZone.then(hostedZone => hostedZone.zoneId),
+  type: 'A',
+  aliases: [{
+    name: cdn.domainName,
+    zoneId: cdn.hostedZoneId,
+    evaluateTargetHealth: true
+  }]
+})
+
+const bucketPolicy = new aws.s3.BucketPolicy('bucketPolicy', {
+  bucket: siteBucket.id, // refer to the bucket created earlier
+  policy: pulumi.all([originAccessIdentity.iamArn, siteBucket.arn]).apply(([oaiArn, bucketArn]) => JSON.stringify({
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Principal: {
+          AWS: oaiArn
+        }, // Only allow Cloudfront read access.
+        Action: ['s3:GetObject'],
+        Resource: [`${bucketArn}/*`] // Give Cloudfront access to the entire bucket.
+      }
+    ]
+  }))
+})
+
+export const recordId = record.id
+export const bucketName = siteBucket.id
+export const bucketPolicyId = bucketPolicy.id
+export const contentBucketUri = pulumi.interpolate`s3://${siteBucket.bucket}`
+export const contentBucketWebsiteEndpoint = siteBucket.websiteEndpoint
+export const distributionId = cdn.id
+export const cloudFrontDomain = cdn.domainName
+export const targetDomainEndpoint = `https://${domain}/`
