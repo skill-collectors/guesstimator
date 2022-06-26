9a4194cee3b8a86293c3c5aef6557f6b499e122e
Dave Burke <dburke84@gmail.com>
2022-06-26 12:38:00 -0500

    Tweak styles

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -6,30 +6,53 @@
     Visit a <a class="link" href="example.com">Link</a>
   </p>
   <form>
-    <fieldset>
-      <legend class="font-bold">Input types</legend>
-      <label for="text">Text:</label>
-      <input name="text" type="text" value="text input" />
-
-      <label for="checkbox">Checkbox:</label>
-      <input name="checkbox" type="checkbox" checked />
-      <input name="checkbox2" type="checkbox" />
+    <fieldset class="flex flex-col">
+      <legend class="font-bold mb-2">Input types</legend>
+      <div class="paragraph">
+        <label for="text">Text:</label>
+        <input name="text" type="text" class="text-input" value="text input" />
+      </div>
+      <div class="paragraph">
+        <label for="number">Numeric:</label>
+        <input
+          name="number"
+          type="numeric"
+          class="text-input"
+          min="0"
+          max="10"
+          step="1"
+          value="1"
+        />
+      </div>
+      <div class="paragraph">
+        <label for="slider">Slider:</label>
+        <input name="slider" type="range" />
+      </div>
+      <div class="paragraph">
+        <label for="checkbox">Checkbox:</label>
+        <input name="checkbox" type="checkbox" checked />
+        <input name="checkbox2" type="checkbox" />
+      </div>
     </fieldset>
-    <label for="select">Select:</label>
-    <select name="select">
-      <option>One</option>
-      <option>Two</option>
-    </select>
-    <ul>
-      <li>
-        <input type="radio" id="one" value="1" name="radio" />
-        <label for="one">One</label>
-      </li>
-      <li>
-        <input type="radio" id="two" value="2" name="radio" />
-        <label for="two">Two</label>
-      </li>
-    </ul>
+    <div class="paragraph">
+      <label for="select">Select:</label>
+      <select name="select">
+        <option>One</option>
+        <option>Two</option>
+      </select>
+    </div>
+    <div class="paragraph">
+      <ul>
+        <li>
+          <input type="radio" id="one" value="1" name="radio" />
+          <label for="one">One</label>
+        </li>
+        <li>
+          <input type="radio" id="two" value="2" name="radio" />
+          <label for="two">Two</label>
+        </li>
+      </ul>
+    </div>
     <button class="btn btn-primary">Primary</button>
     <button class="btn btn-secondary">Secondary</button>
     <button class="btn btn-danger">Danger</button>
--- frontend/windi.config.ts
+++ frontend/windi.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "windicss/helpers";
-import colors from "windicss/colors";
 
 export default defineConfig({
   shortcuts: {
+    "text-input": "rounded border border-gray-400 pl-1",
     btn: "rounded font-semibold p-2",
     "btn-primary": "bg-sky-700 text-sky-50",
     "btn-secondary": "bg-stone-800 text-stone-50",
     "btn-danger": "bg-red-800 text-red-50",
-    heading: "font-bold font-sans",
+    heading: "font-bold font-sans mb-2",
     "h-title": "uppercase text-6xl",
     "h-main": "text-4xl",
     "h-sub": "text-2xl text-gray-700",
+    paragraph: "mb-2",
     link: "text-sky-600 font-medium",
   },
 });
