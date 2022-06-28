8183f3abacaeaca88e4ef33d3c1623e06f4a6e77
Dave Burke <dburke84@gmail.com>
2022-06-27 19:01:52 -0500

    Verify deep links with playwright

--- frontend/tests/e2e/test.ts
+++ frontend/tests/e2e/test.ts
@@ -4,3 +4,8 @@ test("index page has expected h1", async ({ page }) => {
   await page.goto("/");
   expect(await page.textContent("h1")).toBe("Agile Planning Poker");
 });
+
+test("deep links to non-root pages work", async ({ page }) => {
+  const response = await page.goto("/style");
+  expect(response?.status()).toBe(200);
+});
