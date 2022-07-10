137ee18ba2c46708e250dcc6ee2f703bfcc0f2ef
Dave Burke <dburke84@gmail.com>
2022-07-10 14:29:12 -0500

    Fix playwright

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import WindiCSS from "vite-plugin-windicss";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit(), WindiCSS()],
+  preview: {
+    host: "localhost",
+    port: 3001, // Don't conflict with dev server, if running
+  },
   test: {
     globals: true,
     environment: "happy-dom",
