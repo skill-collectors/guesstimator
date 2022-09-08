cb333a0e24ffa52825cd82897de32a83ebe91ccb
Dave Burke <dburke84@gmail.com>
2022-09-07 19:05:03 -0500

    Call api

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,21 @@
-<h2 class="heading h-main">Home</h2>
-<p class="paragraph">
-  This is the future home of the agile planning poker app.
-</p>
+<script>
+  async function createNewRoom() {
+    console.log("click!");
+    const response = await fetch(
+      "https://agile-poker-api-dev.superfun.link/rooms/new"
+    );
+    if (response.ok) {
+      console.log(await response.json());
+    } else {
+      console.log(await response.text());
+    }
+  }
+</script>
+
+<div class="text-center">
+  <p class="paragraph mb-3">
+    This is an app for "pointing" agile stories. To get started
+  </p>
+  <button on:click={createNewRoom} class="btn btn-primary">Create a room</button
+  >
+</div>
