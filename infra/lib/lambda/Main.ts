661f8ecd82af4f58a2e614544e513f52c25c5a7b
Dave Burke <dburke84@gmail.com>
2022-10-27 07:14:21 -0500

    Move router initialization into lambda
    Otherwise Pulumi complains that we are calling ".get()" on the table
    name Output while the stack is still being created.
    
    This means the routes and cors rules will be created for every request,
    but the performance penalty should be negligible.

--- infra/lib/lambda/Main.ts
+++ infra/lib/lambda/Main.ts
@@ -4,23 +4,22 @@ import { corsRules } from "./rest/CorsPlugin";
 import { initRouter } from "./rest/Router";
 
 export function createRouter(tableName: pulumi.Output<string>) {
-  const corsPlugin = corsRules({
-    allowedOrigins: [
-      "http://localhost",
-      "http://127.0.0.1",
-      "https://guesstimator-dev.superfun.link",
-      "https://guesstimator-qa.superfun.link",
-      "https://guesstimator.superfun.link",
-    ],
-    allowedHeaders: ["x-api-key"],
-    allowedMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  });
-
-  const router = initRouter(tableName.get());
-
-  const main = corsPlugin.applyTo(router.run);
-
   return async function (event: APIGatewayProxyEvent) {
+    const corsPlugin = corsRules({
+      allowedOrigins: [
+        "http://localhost",
+        "http://127.0.0.1",
+        "https://guesstimator-dev.superfun.link",
+        "https://guesstimator-qa.superfun.link",
+        "https://guesstimator.superfun.link",
+      ],
+      allowedHeaders: ["x-api-key"],
+      allowedMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    });
+
+    const router = initRouter(tableName.get());
+
+    const main = corsPlugin.applyTo(router.run);
     return main(event);
   };
 }
