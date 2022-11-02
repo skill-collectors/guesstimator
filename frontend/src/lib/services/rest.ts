cab5124374a67d1c329784abd46b559d964238db
Dave Burke <dburke84@gmail.com>
2022-11-01 19:43:36 -0500

    Make frontend fetch wrapper more robust
    We will still need a standard way of displaying errors to the user when
    we catch them (and even if we don't).

--- frontend/src/lib/services/rest.ts
+++ frontend/src/lib/services/rest.ts
@@ -3,90 +3,202 @@
  * - applies the baseUrl
  * - always uses CORS
  * - assumes JSON responses.
- * - throws errors if the response is not ok
+ * - includes the apiKey in all calls if present
+ * - throws errors if the response is 4xx or 5xx
  */
 
 const baseUrl = import.meta.env.VITE_PUBLIC_API_URL;
 const apiKey = import.meta.env.VITE_PUBLIC_API_KEY;
 
-function headers() {
-  const baseHeaders = new Headers();
-  baseHeaders.append("x-api-key", apiKey);
-  return baseHeaders;
+/**
+ * Wraps fetch() with the assumptions described in the jsdoc at the top of this file.
+ *
+ * @param method The request method, e.g. "GET" or "POST".
+ * @param path The path of the request, not including the baseUrl prefix.
+ * @param options Additional options to pass to fetch()
+ * @param headers Any headers to add to the request. This method automatically includes the correct 'x-api-key'.
+ * @returns The result of the fetch call.
+ */
+async function doFetch(
+  method: string,
+  path: string,
+  body: object | null,
+  options: RequestInit,
+  headers: Headers
+) {
+  headers.append("x-api-key", apiKey);
+  const response = await fetch(`${baseUrl}${path}`, {
+    ...options,
+    headers,
+    body: body === null ? null : JSON.stringify(body),
+    method,
+    mode: "cors",
+  });
+  await throwIfNotOk(method, path, response);
+  const json = await response.json();
+  return json;
 }
 
-async function handleFailure(path: string, response: Response) {
+/**
+ * Performs a GET request for the given path.
+ * @param path The path, not including the API baseUrl, e.g. "/rooms".
+ * @param options Any additional options to pass to fetch().
+ * @param headers Any headers to include in the request. 'x-api-key' is always included and need not be passed to this method.
+ * @returns The result of the fetch().
+ * @throws {ServerOverLoadedError} If the server returns 429
+ * @throws {NotFoundError} If the server returns 404
+ * @throws {ServerErrorWithId} If the server returns 500 with an 'errorId' on the response JSON.
+ * @throws {UnknownServerError} If the server returns something unexpected.
+ */
+export async function get(
+  path: string,
+  options: RequestInit = {},
+  headers = new Headers()
+) {
+  const body = null; // GET requests can't contain a body.
+  return doFetch("GET", path, body, options, headers);
+}
+
+/**
+ * Performs a POST request for the given path.
+ * @param path The path, not including the API baseUrl, e.g. "/rooms".
+ * @param options Any additional options to pass to fetch().
+ * @param headers Any headers to include in the request. 'x-api-key' is always included and need not be passed to this method.
+ * @returns The result of the fetch().
+ * @throws {ServerOverLoadedError} If the server returns 429
+ * @throws {NotFoundError} If the server returns 404
+ * @throws {ServerErrorWithId} If the server returns 500 with an 'errorId' on the response JSON.
+ * @throws {UnknownServerError} If the server returns something unexpected.
+ */
+export async function post(
+  path: string,
+  body: object = {},
+  options: RequestInit = {},
+  headers = new Headers()
+) {
+  return doFetch("POST", path, body, options, headers);
+}
+
+/**
+ * Performs a DELETE request for the given path.
+ * @param path The path, not including the API baseUrl, e.g. "/rooms".
+ * @param options Any additional options to pass to fetch().
+ * @param headers Any headers to include in the request. 'x-api-key' is always included and need not be passed to this method.
+ * @returns The result of the fetch().
+ * @throws {ServerOverLoadedError} If the server returns 429
+ * @throws {NotFoundError} If the server returns 404
+ * @throws {ServerErrorWithId} If the server returns 500 with an 'errorId' on the response JSON.
+ * @throws {UnknownServerError} If the server returns something unexpected.
+ */
+export async function del(
+  path: string,
+  options: RequestInit = {},
+  headers = new Headers()
+) {
+  return doFetch("DELETE", path, null, options, headers);
+}
+
+/**
+ * If the given response is not "OK" (status is not 2xx), then this method
+ * throws an appropriate Error.
+ *
+ * If the given response is ok, then this function does nothing.
+ *
+ * @param method The method that was called, e.g. "GET" or "POST".
+ * @param path The path requested from the server.
+ * @param response The response received from "fetch".
+ * @throws {ServerOverLoadedError} If the server returns 429
+ * @throws {NotFoundError} If the server returns 404
+ * @throws {ServerErrorWithId} If the server returns 500 with an 'errorId' on the response JSON.
+ * @throws {UnknownServerError} If the server returns something unexpected.
+ */
+async function throwIfNotOk(method: string, path: string, response: Response) {
   if (!response.ok) {
     if (response.status === 429) {
-      console.log("Server is overloaded!");
-      throw new Error("Server is overloaded!");
+      throw new ServerOverLoadedError();
+    } else if (response.status === 404) {
+      throw new NotFoundError(method, path, await response.text());
     } else {
-      throw new Error(
-        `${path} => ${response.status} (${
-          response.statusText
-        }): ${await response.text()}`
-      );
+      try {
+        const json = await response.json();
+        if ("errorId" in json) {
+          const { errorId, timestamp } = json;
+          throw new ServerErrorWithId(method, path, errorId, timestamp);
+        }
+      } catch (err) {
+        // In case of errors parsing JSON on the response
+        throw new UnknownServerError(method, path, await response.text());
+      }
     }
+    throw new UnknownServerError(method, path, await response.text());
   }
 }
 
-export async function get(path: string, options: RequestInit = {}) {
-  try {
-    const response = await fetch(`${baseUrl}${path}`, {
-      ...options,
-      headers: headers(),
-      method: "GET",
-      mode: "cors",
-    });
-    await handleFailure(path, response);
-    const json = await response.json();
-    return json;
-  } catch (err) {
-    // This happens when the API Gateway responds with 429 status, but WITHOUT
-    // the CORS headers. In that case we can't see the response (including the
-    // 429 status). It's unknown if this happens in other situations.
-    // This is an opportunity to improve the code so we don't need this catch.
-    console.log(err);
-    throw err;
+/* ******************************************************************
+ * The following classes exist so that frontend code can use 'typeof'
+ * expressions to determine what went wrong.
+ * ******************************************************************/
+
+/**
+ * Thrown if the gateway response with 429 because the usage plan was exceeded.
+ */
+export class ServerOverLoadedError extends Error {
+  constructor() {
+    super("The API rate limit is exceeded or the quota has been reached.");
+
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, ServerOverLoadedError.prototype);
   }
 }
 
-export async function post(path: string, options: RequestInit = {}) {
-  try {
-    const response = await fetch(`${baseUrl}${path}`, {
-      ...options,
-      headers: headers(),
-      method: "POST",
-      mode: "cors",
-    });
-    await handleFailure(path, response);
-    return response.json();
-  } catch (err) {
-    // This happens when the API Gateway responds with 429 status, but WITHOUT
-    // the CORS headers. In that case we can't see the response (including the
-    // 429 status). It's unknown if this happens in other situations.
-    // This is an opportunity to improve the code so we don't need this catch.
-    console.log(err);
-    throw err;
+/**
+ * Thrown if the gateway response with 404. This would happen if the user does
+ * something like request a room that no longer exists.
+ */
+export class NotFoundError extends Error {
+  constructor(method: string, path: string, responseText: string) {
+    super(`${method} ${path} => 404 (NOT FOUND): ${responseText}`);
+
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 }
 
-export async function del(path: string, options: RequestInit = {}) {
-  try {
-    const response = await fetch(`${baseUrl}${path}`, {
-      ...options,
-      headers: headers(),
-      method: "DELETE",
-      mode: "cors",
-    });
-    await handleFailure(path, response);
-    return response.json();
-  } catch (err) {
-    // This happens when the API Gateway responds with 429 status, but WITHOUT
-    // the CORS headers. In that case we can't see the response (including the
-    // 429 status). It's unknown if this happens in other situations.
-    // This is an opportunity to improve the code so we don't need this catch.
-    console.log(err);
-    throw err;
+/**
+ * Thrown if the server responds with a 500 and includes an 'errorId'. The
+ * errorId and timestamp should be given to the user so they can include it if
+ * they create an issue in GitHub.
+ */
+export class ServerErrorWithId extends Error {
+  errorId;
+  timestamp;
+
+  constructor(
+    method: string,
+    path: string,
+    errorId: string,
+    timestamp: string
+  ) {
+    super(
+      `${timestamp}: ${method} ${path} => 500 (Internal Server Error): ${errorId}`
+    );
+    this.errorId = errorId;
+    this.timestamp = timestamp;
+
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, ServerErrorWithId.prototype);
+  }
+}
+
+/**
+ * Thrown if we get something other than the other defined error types. This
+ * should never happen normally and probably represents a programming error.
+ */
+export class UnknownServerError extends Error {
+  constructor(method: string, path: string, responseText: string) {
+    super(`${method} ${path} => 500 (Internal Server Error): ${responseText}`);
+
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, UnknownServerError.prototype);
   }
 }
