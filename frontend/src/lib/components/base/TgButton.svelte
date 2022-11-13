fe72365329eb0e326c67a60018d815692e3e3081
Dave Burke <dburke84@gmail.com>
2022-11-13 12:24:44 -0600

    Allow href for a tags in TgButton

--- frontend/src/lib/components/base/TgButton.svelte
+++ frontend/src/lib/components/base/TgButton.svelte
@@ -1,24 +1,48 @@
 <script type="ts">
   export let type: "primary" | "secondary" | "danger" | "success";
+  export let href: string;
 </script>
 
-{#if type === "primary"}
-  <button class="rounded font-semibold p-2 bg-blue-900 text-blue-50"
-    ><slot /></button
-  >
-{/if}
-{#if type === "secondary"}
-  <button class="rounded font-semibold p-2 bg-slate-800 text-slate-50"
-    ><slot /></button
-  >
-{/if}
-{#if type === "danger"}
-  <button class="rounded font-semibold p-2 bg-orange-800 text-orange-50"
-    ><slot /></button
-  >
-{/if}
-{#if type === "success"}
-  <button class="rounded font-semibold p-2 bg-lime-800 text-lime-50"
-    ><slot /></button
-  >
+{#if href === undefined}
+  {#if type === "primary"}
+    <button class="rounded font-semibold p-2 bg-blue-900 text-blue-50"
+      ><slot /></button
+    >
+  {/if}
+  {#if type === "secondary"}
+    <button class="rounded font-semibold p-2 bg-slate-800 text-slate-50"
+      ><slot /></button
+    >
+  {/if}
+  {#if type === "danger"}
+    <button class="rounded font-semibold p-2 bg-orange-800 text-orange-50"
+      ><slot /></button
+    >
+  {/if}
+  {#if type === "success"}
+    <button class="rounded font-semibold p-2 bg-lime-800 text-lime-50"
+      ><slot /></button
+    >
+  {/if}
+{:else}
+  {#if type === "primary"}
+    <a {href} class="rounded font-semibold p-2 bg-blue-900 text-blue-50"
+      ><slot /></a
+    >
+  {/if}
+  {#if type === "secondary"}
+    <a {href} class="rounded font-semibold p-2 bg-slate-800 text-slate-50"
+      ><slot /></a
+    >
+  {/if}
+  {#if type === "danger"}
+    <a {href} class="rounded font-semibold p-2 bg-orange-800 text-orange-50"
+      ><slot /></a
+    >
+  {/if}
+  {#if type === "success"}
+    <a {href} class="rounded font-semibold p-2 bg-lime-800 text-lime-50"
+      ><slot /></a
+    >
+  {/if}
 {/if}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import TgButton from "$lib/components/base/TgButton.svelte";
   import TgHeadingMain from "$lib/components/base/TgHeadingMain.svelte";
   import TgHeadingTitle from "$lib/components/base/TgHeadingTitle.svelte";
   import TgParagraph from "$lib/components/base/TgParagraph.svelte";
@@ -10,5 +11,5 @@
   <TgParagraph>
     This is an app for "pointing" agile stories. To get started
   </TgParagraph>
-  <a class="btn primary" href="/rooms">Create a room</a>
+  <TgButton type="primary" href="/rooms">Create a room</TgButton>
 </div>
