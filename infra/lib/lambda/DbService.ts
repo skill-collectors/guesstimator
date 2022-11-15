25ffc9ec9baad300c9e48851cd3fa209ac986025
Dave Burke <dburke84@gmail.com>
2022-11-15 17:59:21 -0600

    Delete rooms by PK (including all SK rows)

--- infra/lib/lambda/DbService.ts
+++ infra/lib/lambda/DbService.ts
@@ -5,6 +5,27 @@ import * as aws from "@pulumi/aws";
 const ROOM_ID_LENGTH = 6;
 const HOST_KEY_LENGTH = 4;
 
+async function queryAndDelete(
+  client: DocumentClient,
+  tableName: string,
+  params: DocumentClient.QueryInput
+) {
+  const queryOutput = await client.query(params).promise();
+  const lastEvaluatedKey = queryOutput.LastEvaluatedKey;
+  const deleteRequests =
+    queryOutput.Items?.map((item) => ({
+      DeleteRequest: { Key: { PK: item.PK, SK: item.SK } },
+    })) || [];
+  await client
+    .batchWrite({
+      RequestItems: {
+        [tableName]: deleteRequests,
+      },
+    })
+    .promise();
+  return lastEvaluatedKey;
+}
+
 export default class DbService {
   client: DocumentClient;
   tableName: string;
@@ -70,12 +91,25 @@ export default class DbService {
 
     return response;
   }
+
   async deleteRoom(roomId: string) {
-    await this.client
-      .delete({
-        TableName: this.tableName,
-        Key: { PK: `ROOM:${roomId}`, SK: "ROOM" },
-      })
-      .promise();
+    const params = {
+      TableName: this.tableName,
+      KeyConditionExpression: "PK = :pk",
+      ExpressionAttributeValues: {
+        ":pk": `ROOM:${roomId}`,
+      },
+    };
+    let lastEvaluatedKey = await queryAndDelete(
+      this.client,
+      this.tableName,
+      params
+    );
+    while (typeof lastEvaluatedKey !== "undefined") {
+      lastEvaluatedKey = await queryAndDelete(this.client, this.tableName, {
+        ...params,
+        ExclusiveStartKey: lastEvaluatedKey,
+      });
+    }
   }
 }
