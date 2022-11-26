3c0cf9ff2dac82d522cb2fa3fb436253431ccb9f
Dave Burke <dburke84@gmail.com>
2022-11-26 11:22:21 -0600

    Fix card display value reactivity

--- frontend/src/routes/rooms/[roomId]/+page.svelte
+++ frontend/src/routes/rooms/[roomId]/+page.svelte
@@ -142,7 +142,7 @@
         {/if}
       {/if}
     </TgParagraph>
-    {#each roomData?.users as user}
+    {#each roomData?.users as user (user.username)}
       <Card
         username={user.username}
         isRevealed={roomData.isRevealed}
