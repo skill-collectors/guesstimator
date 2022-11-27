a93db55dd35a7811cba6671eea2a290233472a7a
Dave Burke <dburke84@gmail.com>
2022-11-27 13:15:05 -0600

    Reload room immediately after update actions
    This makes the page a little more responsive. It's not perfect, but it's
    only a temporary solution until we implement websockets.

--- frontend/src/routes/rooms/[roomId]/+page.svelte
+++ frontend/src/routes/rooms/[roomId]/+page.svelte
@@ -8,7 +8,7 @@
   import { redirectToErrorPage } from "$lib/services/errorHandler";
   import * as localStorage from "$lib/services/localStorage";
   import { ApiEndpointNotFoundError } from "$lib/services/rest";
-  import type { Room } from "$lib/services/rooms";
+  import type { Room, User } from "$lib/services/rooms";
   import * as rooms from "$lib/services/rooms";
   import { onMount, onDestroy } from "svelte";
   import InvalidRoom from "./InvalidRoom.svelte";
@@ -16,9 +16,10 @@
   let notFound = false;
   const roomId = $page.params.roomId;
   const url = $page.url;
+
   let hostKey: string | null = null;
   let userKey: string | null = null;
-  let username = "";
+  let usernameFieldValue = "";
   let roomData: Room | null = null;
   let reloadIntervalId: number | undefined;
 
@@ -28,38 +29,37 @@
 
     const userData = localStorage.getUserData(roomId);
     userKey = userData.userKey;
-    username = userData.username;
 
-    loadRoomData();
-    reloadIntervalId = window.setInterval(
-      async () => await loadRoomData(),
-      2_000
-    );
+    await resetReloadInterval();
   });
 
   onDestroy(() => {
     clearInterval(reloadIntervalId);
   });
 
-  let selectedSize = "";
-
-  async function setSelection(size = "") {
-    if (userKey !== null) {
-      await rooms.vote(roomId, userKey, size);
-      selectedSize = size;
+  /**
+   * Reloads the room immediately and resets the reload interval.
+   * @param action An optional action to perform while reloading is suspended.
+   */
+  async function resetReloadInterval(action?: () => Promise<void>) {
+    window.clearInterval(reloadIntervalId);
+    reloadIntervalId = undefined;
+    if (action !== undefined) {
+      await action();
+    }
+    await loadRoomData();
+    // Don't create a second interval if one is already defined
+    if (reloadIntervalId === undefined) {
+      reloadIntervalId = window.setInterval(
+        async () => await loadRoomData(),
+        2_000
+      );
     }
   }
 
   async function loadRoomData() {
     try {
       roomData = await rooms.getRoom(roomId, userKey);
-      const currentUser = roomData.users.find(
-        (user) => user.userKey !== undefined && user.userKey === userKey
-      );
-      if (currentUser !== undefined) {
-        username = currentUser.username;
-        selectedSize = currentUser.vote;
-      }
     } catch (err) {
       if (err instanceof ApiEndpointNotFoundError) {
         notFound = true;
@@ -67,6 +67,47 @@
         redirectToErrorPage(err);
       }
     }
+  }
+
+  let currentUser: User | undefined;
+  $: {
+    currentUser = roomData?.users.find(
+      (user) => user.userKey !== undefined && user.userKey === userKey
+    );
+  }
+
+  async function handleJoinRoomClick() {
+    await resetReloadInterval(async () => {
+      if (roomData !== null) {
+        const result = await rooms.joinRoom(
+          roomData.roomId,
+          usernameFieldValue
+        );
+        userKey = result.userKey;
+        localStorage.storeUserData(
+          result.roomId,
+          result.userKey,
+          usernameFieldValue
+        );
+      }
+    });
+  }
+
+  async function setSelection(size = "") {
+    await resetReloadInterval(async () => {
+      if (currentUser !== undefined && userKey !== null) {
+        currentUser.vote = size;
+        await rooms.vote(roomId, userKey, size);
+      }
+    });
+  }
+
+  async function setIsRevealed(isRevealed: boolean) {
+    await resetReloadInterval(async () => {
+      if (roomData !== null && hostKey !== null) {
+        await rooms.setIsRevealed(roomData.roomId, isRevealed, hostKey);
+      }
+    });
   }
 
   async function handleDeleteRoom() {
@@ -80,25 +121,6 @@
     await rooms.deleteRoom(roomData.roomId, hostKey);
     localStorage.deleteHostKey(roomData.roomId);
     window.location.href = "/";
-  }
-
-  async function handleJoinRoomClick() {
-    if (roomData !== null) {
-      const result = await rooms.joinRoom(roomData.roomId, username);
-      userKey = result.userKey;
-      localStorage.storeUserData(result.roomId, result.userKey, username);
-    }
-  }
-
-  async function setIsRevealed(isRevealed: boolean) {
-    if (roomData === null) {
-      return;
-    }
-    if (hostKey === null) {
-      return;
-    }
-    await rooms.setIsRevealed(roomData.roomId, isRevealed, hostKey);
-    roomData.isRevealed = isRevealed;
   }
 </script>
 
@@ -153,10 +175,10 @@
   </section>
   {#if !roomData?.isRevealed}
     <section class="mt-32">
-      {#if userKey}
+      {#if currentUser !== undefined}
         <TgHeadingSub>Your votes:</TgHeadingSub>
         {#each roomData.validSizes as size}
-          {#if size === selectedSize}
+          {#if size === currentUser.vote}
             <TgButton type="primary" class="m-2" on:click={() => setSelection()}
               >{size}</TgButton
             >
@@ -171,12 +193,16 @@
         <TgButton type="secondary" class="m-2" on:click={() => setSelection("")}
           >Clear</TgButton
         >
-        <TgParagraph>You are joined as {username}</TgParagraph>
+        <TgParagraph>You are joined as {currentUser.username}</TgParagraph>
       {:else}
         <TgParagraph
           >If you'd like to vote, enter a name and join the room:</TgParagraph
         >
-        <TgInputText name="newUser" maxlength={30} bind:value={username} />
+        <TgInputText
+          name="newUser"
+          maxlength={30}
+          bind:value={usernameFieldValue}
+        />
         <TgButton type="primary" on:click={handleJoinRoomClick}
           >Join room</TgButton
         >
