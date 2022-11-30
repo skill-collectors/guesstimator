f5e840e34fd1f62d2e2db794b0230803ac748d92
Dave Burke <dburke84@gmail.com>
2022-11-30 17:16:07 -0600

    Sort local logs descending
    Otherwise the --limit 50 cuts off recent logs.

--- local-logs-list.sh
+++ local-logs-list.sh
@@ -5,7 +5,7 @@ set -e
 logGroups="$(awslocal logs describe-log-groups --query 'logGroups[*].logGroupName' | jq -r '.[]')"
 
 while IFS= read -r logGroup; do
-  logStreams="$(awslocal logs describe-log-streams --log-group-name ${logGroup} --query 'logStreams[*].logStreamName' --limit 50 | jq -r '.[]')"
+  logStreams="$(awslocal logs describe-log-streams --log-group-name ${logGroup} --query 'logStreams[*].logStreamName' --descending --limit 50 | jq -r '.[]')"
   while IFS= read -r logStream; do
     awslocal logs get-log-events --log-group-name "${logGroup}" --log-stream-name "${logStream}"
   done <<< "${logStreams}"
