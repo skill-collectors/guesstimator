5094ed4117dea4ebb5fade5e2150f758f40fd24d
Dave Burke <dburke84@gmail.com>
2022-12-06 16:14:22 -0600

    Animate cards

--- frontend/src/lib/components/Card.svelte
+++ frontend/src/lib/components/Card.svelte
@@ -4,13 +4,20 @@
   export let hasValue = false;
   export let value = "";
 
+  let delay = 0;
+  $: if (isRevealed === true) {
+    // Set a random delay for each reveal
+    // This makes it feel more natural vs all cards flipping in perfect synchrony.
+    delay = Math.floor(Math.random() * 5) + 1;
+  }
+
   $: displayValue = value === "" ? "?" : value;
 </script>
 
 <div class="inline-block animate-animated animate-fade-in">
   {#if isRevealed}
     <div
-      class="animate-animated animate-flip-in-y shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 md:w-36 md:h-56 p-2 m-2 font-bold bg-teal-50 rounded-lg border-4 border-teal-600 text-teal-700"
+      class="animate-animated animate-flip-in-y animate-delay-{delay}00 shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 md:w-36 md:h-56 p-2 m-2 font-bold bg-teal-50 rounded-lg border-4 border-teal-600 text-teal-700"
     >
       <div class="text-3xl md:text-6xl">{displayValue}</div>
       <div>{username}</div>
--- frontend/windi.config.ts
+++ frontend/windi.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from "windicss/helpers";
 import { default as pluginAnimations } from "@windicss/plugin-animations";
 
 export default defineConfig({
+  // Entries in the safelist are required for any dynamic classes
+  // See https://windicss.org/integrations/svelte.html#safe-list
+  safelist: [
+    "animate-delay-100",
+    "animate-delay-200",
+    "animate-delay-300",
+    "animate-delay-400",
+    "animate-delay-500",
+  ],
   plugins: [
     pluginAnimations({
       settings: {
