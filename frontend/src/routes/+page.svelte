e215ad93c2d4bbad089ef59181e494bdefeb8f0a
Mike Rugh <mike.rugh@gmail.com>
2023-01-26 11:34:18 -0800

    fixes styling

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -31,13 +31,6 @@
       >Create a room</TgButton
     >
   {:else}
-    <div id="api-check">API IS UNAVAILABLE</div>
+    <div id="api-check" class="text-red-500 font-bold">API IS UNAVAILABLE</div>
   {/if}
 </div>
-
-<style>
-  #api-check {
-    color: red;
-    font-weight: bold;
-  }
-</style>
