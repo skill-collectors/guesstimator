164e2b61bab35ad1082cfdc72ca301c5198f2479
Mike Rugh <mike.rugh@gmail.com>
2023-01-26 11:30:13 -0800

    adds api status check and error display

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,6 +3,21 @@
   import TgHeadingMain from "$lib/components/base/TgHeadingMain.svelte";
   import TgHeadingTitle from "$lib/components/base/TgHeadingTitle.svelte";
   import TgParagraph from "$lib/components/base/TgParagraph.svelte";
+  import { get } from "$lib/services/rest";
+  import { onMount } from "svelte";
+
+  var status = true;
+
+  onMount(async () => {
+    try {
+      const res = await get("status");
+      if (res.status !== "UP") {
+        status = false;
+      }
+    } catch (err) {
+      console.error("err: ", err);
+    }
+  });
 </script>
 
 <div class="text-center">
@@ -11,7 +26,18 @@
   <TgParagraph>
     This is an app for "pointing" agile stories. To get started
   </TgParagraph>
-  <TgButton id="createRoomButton" type="primary" href="/rooms" class="mt-4"
-    >Create a room</TgButton
-  >
+  {#if status}
+    <TgButton id="createRoomButton" type="primary" href="/rooms" class="mt-4"
+      >Create a room</TgButton
+    >
+  {:else}
+    <div id="api-check">API IS UNAVAILABLE</div>
+  {/if}
 </div>
+
+<style>
+  #api-check {
+    color: red;
+    font-weight: bold;
+  }
+</style>
