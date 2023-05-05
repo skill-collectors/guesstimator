478a1cc309e58a695b46fb1f5091f2075334e381
Dave Burke <dburke84@gmail.com>
2023-05-04 20:49:03 -0500

    Open footer links in _blank

--- frontend/src/lib/components/PageFooter.svelte
+++ frontend/src/lib/components/PageFooter.svelte
@@ -7,20 +7,28 @@
 <footer class="text-center p-3">
   <TgLink href="/">home</TgLink>
   |
-  <TgLink href="https://github.com/skill-collectors/guesstimator">
+  <TgLink
+    href="https://github.com/skill-collectors/guesstimator"
+    target="_blank"
+  >
     source code
   </TgLink>
   (AGPL) |
-  <TgLink href="https://skill-collectors.github.io/guesstimator">
+  <TgLink
+    href="https://skill-collectors.github.io/guesstimator"
+    target="_blank"
+  >
     dev docs |
   </TgLink>
   <TgLink
     href="https://github.com/skill-collectors/guesstimator/issues/new?assignees=&labels=bug&template=bug_report.md&title="
+    target="_blank"
   >
     report a bug |
   </TgLink>
   <TgLink
     href="https://github.com/skill-collectors/guesstimator/issues/new?assignees=&labels=enhancement&template=feature_request.md&title="
+    target="_blank"
   >
     feature request
   </TgLink>
--- frontend/src/lib/components/base/TgLink.svelte
+++ frontend/src/lib/components/base/TgLink.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   export let href: string;
+  export let target: "_self" | "_blank" | "_parent" | "_top" = "_self";
   let additionalClasses = "";
   export { additionalClasses as class };
 </script>
 
-<a {href} class={`text-blue-800 font-medium ${additionalClasses}`}><slot /></a>
+<a {href} {target} class={`text-blue-800 font-medium ${additionalClasses}`}
+  ><slot /></a
+>
