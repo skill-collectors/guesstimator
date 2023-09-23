5889b620f7dd65d16b8b3afdcf07912b59aeb746
Dave Burke <dburke84@gmail.com>
2023-09-23 11:43:23 -0500

    Shrink cards at sm instead of md

--- frontend/src/lib/components/Card.svelte
+++ frontend/src/lib/components/Card.svelte
@@ -17,23 +17,23 @@
 <div class="animate-animated animate-fade-in">
   {#if isRevealed}
     <div
-      class="animate-animated animate-flip-in-y animate-delay-{delay}00 shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 md:w-36 md:h-56 p-2 m-2 overflow-hidden break-all font-bold bg-teal-50 rounded-lg border-4 border-teal-600 text-teal-700"
+      class="animate-animated animate-flip-in-y animate-delay-{delay}00 shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 sm:w-36 sm:h-56 p-2 m-2 overflow-hidden break-all font-bold bg-teal-50 rounded-lg border-4 border-teal-600 text-teal-700"
     >
-      <div class="text-3xl md:text-6xl">{displayValue}</div>
+      <div class="text-3xl sm:text-6xl">{displayValue}</div>
       <div>{username}</div>
     </div>
   {:else if hasValue}
     <div
-      class="animate-animated animate-infinite animate-pulse shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 md:w-36 md:h-56 p-2 m-2 overflow-hidden break-all font-bold bg-teal-700 rounded-lg border-5 border-teal-900 text-teal-50"
+      class="animate-animated animate-infinite animate-pulse shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 sm:w-36 sm:h-56 p-2 m-2 overflow-hidden break-all font-bold bg-teal-700 rounded-lg border-5 border-teal-900 text-teal-50"
     >
-      <div class="text-3xl md:text-6xl text-teal-200">!</div>
+      <div class="text-3xl sm:text-6xl text-teal-200">!</div>
       <div>{username}</div>
     </div>
   {:else}
     <div
-      class="shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 md:w-36 md:h-56 p-2 m-2 overflow-hidden break-all font-bold bg-teal-700 rounded-lg border-5 border-teal-900 text-teal-50"
+      class="shadow-dark-100 shadow-lg flex flex-col items-center justify-center w-18 h-28 sm:w-36 sm:h-56 p-2 m-2 overflow-hidden break-all font-bold bg-teal-700 rounded-lg border-5 border-teal-900 text-teal-50"
     >
-      <div class="text-3xl md:text-6xl text-teal-200">&nbsp;</div>
+      <div class="text-3xl sm:text-6xl text-teal-200">&nbsp;</div>
       <div>{username}</div>
     </div>
   {/if}
