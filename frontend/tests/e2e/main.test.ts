e54e6c2068e2406a6ca42d615884dfdc6da5d429
Dave Burke <dburke84@gmail.com>
2024-02-11 16:41:04 -0600

    Verify votes

--- frontend/tests/e2e/main.test.ts
+++ frontend/tests/e2e/main.test.ts
@@ -5,7 +5,7 @@ test("index page has expected h1", async ({ page }) => {
   expect(await page.textContent("h1")).toBe("The Guesstimator");
 });
 
-test("Host workflow", async ({ page, browser }) => {
+test("Basic workflow", async ({ page, browser }) => {
   await page.goto("/");
 
   // Wait for the page to finish rendering, otherwise title is not set
@@ -16,14 +16,16 @@ test("Host workflow", async ({ page, browser }) => {
   await page.waitForSelector("#hostControls");
   expect(await page.title()).toMatch(/Guesstimator - [A-Z0-9]{6}/);
 
-  // User join
-  const userPages = await Promise.all(
+  // Users join
+  const users = await Promise.all(
     ["user1", "user2", "user3", "user4"].map((username) =>
-      joinAs(browser, page.url(), username),
+      addUser(browser, page.url(), username),
     ),
   );
+
+  // Users vote
   await Promise.allSettled(
-    userPages.map((user) => {
+    users.map((user) => {
       const possibleVotes = ["1", "2", "3", "5", "8", "13", "20"];
       const randomIndex = Math.floor(Math.random() * possibleVotes.length);
       const vote = possibleVotes[randomIndex];
@@ -31,17 +33,17 @@ test("Host workflow", async ({ page, browser }) => {
     }),
   );
 
+  // Host joins and votes
   await page.getByRole("textbox").fill("host");
   await page.getByText("Join room").click();
   await page.locator("#userControls").getByText("5").click();
+  await page.getByTestId("host-card").getByText("!").waitFor();
 
   await page.click("#showCardsButton");
   await page.waitForSelector("#resultsChart");
   await page.click("#hideCardsButton");
 
-  await Promise.allSettled(userPages.map((user) => addVote(user, "8")));
-
-  userPages.forEach((user) => user.context().close());
+  users.forEach(async (user) => await user.page.context().close());
 
   await page.click("#deleteRoomButton");
 });
@@ -51,17 +53,18 @@ test("deep links to non-root pages work", async ({ page }) => {
   expect(response?.status()).toBe(200);
 });
 
-async function joinAs(browser: Browser, roomUrl: string, username: string) {
+async function addUser(browser: Browser, roomUrl: string, username: string) {
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto(roomUrl);
   await page.getByRole("textbox").fill(username);
   await page.getByText("Join room").click();
-  await page.locator("#currentVotes").getByText(username).waitFor();
-  expect(page.locator("#currentVotes").getByText(username)).toBeVisible();
-  return page;
+  await page.getByTestId(`${username}-card`).waitFor();
+  console.log(`${username} joined the room`);
+  return { page, username };
 }
 
-async function addVote(page: Page, vote: string) {
-  await page.locator("#userControls").getByText(vote).click();
+async function addVote(user: { page: Page; username: string }, vote: string) {
+  await user.page.locator("#userControls").getByText(vote).click();
+  await user.page.getByTestId(`${user.username}-card`).getByText("!").waitFor();
 }
