06a1c6fa06294afa47f3a826262d8f9e354cb40f
Dave Burke <dburke84@gmail.com>
2025-01-20 21:19:02 -0600

    Use Tailwinds for rainbow animation

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,7 +5,24 @@ import tailwindcssAnimated from 'tailwindcss-animated';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		extend: {
+			// For 'Everyone agreed!' text
+			keyframes: {
+				'rainbow-animation': {
+					'0%': { color: 'red' },
+					'14%': { color: 'orange' },
+					'28%': { color: 'yellow' },
+					'42%': { color: 'green' },
+					'57%': { color: 'blue' },
+					'71%': { color: 'indigo' },
+					'85%': { color: 'violet' },
+					'100%': { color: 'red' },
+				},
+			},
+			animation: {
+				rainbow: 'rainbow-animation 4s infinite',
+			},
+		},
 	},
 	plugins: [tailwindcssAnimated],
 } satisfies Config;
